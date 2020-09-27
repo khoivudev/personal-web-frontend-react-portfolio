c3f399265c5ed5f86f61090fc91e8377c5329241
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-27 22:42:34 +0700

    Call Quote API in landingpage

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,49 +1,81 @@
 import React, {Component} from 'react';
 import { Cell, Grid } from 'react-mdl';
+import axios from 'axios';
 
 class Landing extends Component{
+    constructor(){
+        super();
+        this.state = {
+            quote: null,
+            idLoaded: false,
+        }
+    }
+   
+    componentDidMount(){
+        axios.get('https://api-services-vuhuykhoi.herokuapp.com/api/quote')
+        .then(res => {
+            const quote = res.data;
+            this.setState({
+                isLoaded: true, 
+                quote: quote
+            });
+        })
+        .catch(error => console.log(error));
+    }
+
     render(){
-        return(
-            <div style={{witdh:'100%',margin: 'auto'}}>
-                <div  className="welcome-grid">
-                  <h1>Welcome</h1>
-                  <p>How are you doing, today?</p>
-                </div>
-                <Grid className="about-grid odd-background-color">
-                <Cell col={12}>
-                    <img
-                    src={process.env.REACT_APP_MY_AVATAR_URL}
-                    alt="avatar"
-                    className="avatar-img"
-                    />
+        var {isLoaded, quote} = this.state;
 
-                    <div className="banner-text">
-                        <h1>Hey, I am Khoi</h1>
-                        <p>a web developer</p>
-                        <hr/>
-                        <p>| Skill |</p>
-                        <div className= "social-links">
-                            {/*Github*/}
-                            <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-github" aria-hidden="true"></i>
-                            </a>
-
-                            {/*Facebook*/}
-                            <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-facebook" aria-hidden="true"></i>
-                            </a>
-
-                            {/*Gmail*/}
-                            <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-google" aria-hidden="true"></i>
-                            </a>
+        if(!isLoaded){
+            return <div style={{minHeight: '100vh'}}>Loading...</div>
+        }else{
+            return(
+                <div style={{witdh:'100%',margin: 'auto'}}>
+                    <div  className="welcome-grid">
+                        <h1>Welcome</h1>
+                        <p>How are you doing, today?</p>
+                        <div id="quoteDisplay">
+                            <h3 className ="content">{quote.content}</h3>
+                            <h4 className ="author"> - {quote.author}</h4>
                         </div>
                     </div>
-
-                </Cell>
-                </Grid>
-            </div>
-        );
+                    <Grid className="about-grid odd-background-color">
+                    <Cell col={12}>
+                        <img
+                        src={process.env.REACT_APP_MY_AVATAR_URL}
+                        alt="avatar"
+                        className="avatar-img"
+                        />
+    
+                        <div className="banner-text">
+                            <h1>Hey, I am Khoi</h1>
+                            <p>a web developer</p>
+                            <hr/>
+                            <p>| Skill |</p>
+                            <div className= "social-links">
+                                {/*Github*/}
+                                <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fab fa-github" aria-hidden="true"></i>
+                                </a>
+    
+                                {/*Facebook*/}
+                                <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fab fa-facebook" aria-hidden="true"></i>
+                                </a>
+    
+                                {/*Gmail*/}
+                                <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fab fa-google" aria-hidden="true"></i>
+                                </a>
+                            </div>
+                        </div>
+    
+                    </Cell>
+                    </Grid>
+                </div>
+            );
+        }
+       
     }
 }
 
