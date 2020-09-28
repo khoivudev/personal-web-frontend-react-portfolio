399de29a9e578ce6c76b8463264caa49f2320f37
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-29 00:19:36 +0700

    project page get data from db

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,141 +1,79 @@
 import React, {Component} from 'react';
-import {Tabs, Tab} from 'react-mdl';
+import axios from 'axios';
+
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
+import LoadingIcon from './Loading';
 
 class Projects extends Component{
     constructor(){
         super();
-        this.state = {activeTab: 0 };
+        this.state = {
+            projects: [],
+            idLoaded: false,
+        }
     }
 
-    toggleCategories = ()=> {
-        if(this.state.activeTab === 0){
-          return(
-            <div className="projects-container"><h4>MongoDB projects empty </h4></div>
-        )
-        }else if(this.state.activeTab === 1){
+    componentDidMount(){
+        axios.get('/api/project')
+        .then(res => {
+            const projects = res.data;
+            this.setState({
+                isLoaded: true, 
+                projects: projects
+            });
+        })
+        .catch(error => console.log(error));
+    }
+
+    render(){
+        const {isLoaded, projects} = this.state;
+        if(!isLoaded){
+            return <div className="loading-container"><LoadingIcon/></div>
+        }else{
             return(
-                <div className="projects-container"><h4>ExpressJs projects empty</h4></div>
-            )
-        }else if(this.state.activeTab === 2){
-          return(
-            <div className="projects-container">
+                <div className="projects-container">
                 <Grid container spacing={1}>
-                    <Grid container item xs={12} spacing={4}>
+                    <Grid container item xs={12} spacing={3}>
+                    {projects.map(({name, description, img_url, tech, project_url}) =>(
                         <Grid item xs={4}>
-                            <Card style={{ maxWidth: 345}}>
-                                <CardActionArea>
-                                  <CardMedia
-                                    style={{color:'white', 
-                                    height: "250px", 
-                                    background:'url(https://blogsaays-com-3vkgf8gqdp8entcca1.netdna-ssl.com/wp-content/uploads/2019/05/Todo-list.jpg) center / cover'}}
-                                  />
-          
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                          TodoTask
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                <Button color="primary">Github</Button>
-                                    <Button color="primary">CodePen</Button>
-                                    <Button href="/todotask" color="primary">Demo</Button>
-                                </CardActions>
-                            </Card>
+                        <Card style={{ maxWidth: 345}}>
+                            <CardActionArea>
+                            <CardMedia
+                                style={{color:'white', 
+                                height: "250px", 
+                                background:`url(${img_url}) center / cover`}}
+                            />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                    {name}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        {description}<br/>
+                                        Tech: {tech.map(tech_item => (<span>{tech_item} </span>))}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                            <Button color="primary">Github</Button>
+                                <Button color="primary">CodePen</Button>
+                                <Button href={project_url} color="primary">Demo</Button>
+                            </CardActions>
+                        </Card>
                         </Grid>
-                        <Grid item xs={4}>
-                            <Card style={{ maxWidth: 345}}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        style={{color:'white', 
-                                        height: "250px", 
-                                        background:'url(https://miro.medium.com/max/250/0*CuCS0IzqzdMoAj6W.png) center / cover'}}
-                                    />
-
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                        React Project #2
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-            
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                <Button color="primary">Github</Button>
-                                    <Button color="primary">CodePen</Button>
-                                    <Button href="/todotask" color="primary">Demo</Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Card style={{ maxWidth: 345}}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        style={{color:'white', 
-                                        height: "250px", 
-                                        background:'url(https://miro.medium.com/max/250/0*CuCS0IzqzdMoAj6W.png) center / cover'}}
-                                    />
-
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                        React Project #3
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                          
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                <Button color="primary">Github</Button>
-                                    <Button color="primary">CodePen</Button>
-                                    <Button href="/todotask" color="primary">Demo</Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>                
+                        ))}
+                            
                     </Grid>
                 </Grid>
             </div>
             );
-        }else if(this.state.activeTab === 3){
-            return(
-                <div className="projects-container"><h4>Nodejs projects empty</h4></div>
-            )
         }
-    }
-
-    render(){
-        return(
-            <div className = "category-tabs">
-                <Tabs activeTab={this.state.activeTab} 
-                    onChange={(tabId)=>this.setState({activeTab: tabId})} 
-                    ripple
-                >
-                    <Tab >MongoDB</Tab>
-                    <Tab>ExpressJS</Tab>
-                    <Tab>ReactJs</Tab>
-                    <Tab>NodeJs</Tab>
-                </Tabs> 
-                <section>
-                    <Grid>
-                        <Grid item xs={12}>
-                            <div >{this.toggleCategories()}</div>
-                        </Grid>
-                    </Grid>
-                </section>
-            </div>
-        );
     }
 }
 
